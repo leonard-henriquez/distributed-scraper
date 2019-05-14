faa95508dc39e5c7e0a8d378f311779753aeafc7
Leonard-Henriquez <leonard.henriquez@edhec.com>
2019-05-14 20:04:35 +0200

    Fix upload

--- worker/upload.py
+++ worker/upload.py
@@ -1,12 +1,17 @@
 from hashlib import sha1
 from datetime import datetime
 from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
 
 from .settings import MONGO_URI, MONGO_DATABASE, MONGO_COLLECTION
 
-connection = MongoClient(MONGO_URI)
-db = connection[MONGO_DATABASE]
-collection = db[MONGO_COLLECTION]
+try:
+    connection = MongoClient(MONGO_URI)
+    connection.server_info()
+    db = connection[MONGO_DATABASE]
+    collection = db[MONGO_COLLECTION]
+except ServerSelectionTimeoutError as err:
+    print(err)
 
 def upload_article(article):
   url = article['url']
